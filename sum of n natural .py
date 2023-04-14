5f6d56bce23bc807dad18a47fff53b5ea3bccf15
Shaik-Ibtihajulla-sha <130788843+Shaik-Ibtihajulla-sha@users.noreply.github.com>
2023-04-14 23:57:14 +0530

    Create sum of n natural .py

---
+++ sum of n natural .py
@@ -0,0 +1,10 @@
+num =int(input())
+print(num)
+if num < 0:
+   print("Enter a positive number")
+else:
+   sum = 0
+   while(num > 0):
+       sum += num
+       num -= 1
+   print("The sum is", sum)
